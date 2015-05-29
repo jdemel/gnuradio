19c16279319bc33201555726a7f985b754a3f85f
Johannes Demel <ufcsy@student.kit.edu>
2015-05-29 19:00:39 +0200

    polar: work on SC decoder

--- gr-fec/python/fec/polar/decoder.py
+++ gr-fec/python/fec/polar/decoder.py
@@ -12,9 +12,11 @@ class PolarDecoder(PolarCommon):
     def __init__(self, n, k, frozen_bit_position, frozenbits=None, reverse=True):
         PolarCommon.__init__(self, n, k, frozen_bit_position, frozenbits, reverse)
 
-        self.error_probability = 0.1
-        bsc_error_probs = ((1 - self.error_probability) / self.error_probability, self.error_probability / (1 - self.error_probability))
-        self.bsc_llrs = np.log(bsc_error_probs)
+        self.error_probability = 0.1  # this is kind of a dummy value. usually chosen individually.
+        self.bsc_lr = ((1 - self.error_probability) / self.error_probability, self.error_probability / (1 - self.error_probability))
+        print self.bsc_lr
+        self.bsc_llrs = np.log(self.bsc_lr)
+        print self.bsc_llrs
 
     def _bit_llr(self, bit):
         if type(bit) == np.ndarray:
@@ -28,7 +30,53 @@ class PolarDecoder(PolarCommon):
 
     def _llr_even_func(self, la, lb, f):
         exp_val = float(1 - 2 * f)
-        return (la ** exp_val) * lb
+        return (la * exp_val) + lb
+
+    def _lr_bit(self, bit):
+        return self.bsc_lr[bit]
+
+    def _lr_odd(self, la, lb):
+        # la is upper branch and lb is lower branch
+        return (la * lb + 1) / (la + lb)
+
+    def _lr_even(self, la, lb, f):
+        # la is upper branch and lb is lower branch, f is last decoded bit.
+        return (la ** (1 - (2 * f))) * lb
+
+    def _lr_bit_decision(self, lr):
+        if lr < 1:
+            return int(1)
+        return int(0)
+
+    def _get_even_indices_values(self, u_hat):
+        # looks like overkill for some indexing, but zero and one based indexing mix-up gives you haedaches.
+        return u_hat[1::2]
+
+    def _get_odd_indices_values(self, u_hat):
+        return u_hat[0::2]
+
+    def _calculate_lrs(self, y, u):
+
+        ue = self._get_even_indices_values(u)
+        uo = self._get_odd_indices_values(u)
+        ya = y[0:y.size//2]
+        yb = y[(y.size//2):]
+        print y, '-->', u, 'size', y.size, ya, yb
+        la = self._lr_decision_element(ya, (ue + uo) % 2)
+        lb = self._lr_decision_element(yb, ue)
+
+        return la, lb
+
+    def _lr_decision_element(self, y, u):
+        if y.size == 1:
+            return self._lr_bit(y[0])
+        if u.size % 2 == 0:  # use odd branch formula
+            la, lb = self._calculate_lrs(y, u)
+            return self._lr_odd(la, lb)
+        else:
+            ui = u[-1]
+            la, lb = self._calculate_lrs(y, u)
+            return self._lr_even(la, lb, ui)
 
     def _llr(self, i_in, y, u_hat):
         # i_in is one based for now. Just like in the paper.
@@ -121,12 +169,65 @@ def handle_node(graph, r, e, num):
     return graph, num
 
 
+def decode4():
+    power = 2
+    n = 2 ** power
+    k = n // 2
+    frozenbits = np.zeros(n - k, dtype=int)
+    # frozenbitposition = np.array((0, 1, 2, 4), dtype=int)
+    frozenbitposition = np.array((0, 1), dtype=int)
+
+
+    encoder = PolarEncoder(n, k, frozenbitposition, frozenbits, reverse=True)
+    decoder = PolarDecoder(n, k, frozenbitposition, frozenbits)
+    bits = np.array([1, 1], dtype=int)
+    y = encoder.encode(bits)
+    print y
+    u = np.array([], dtype=int)
+    l1 = decoder._lr_decision_element(y, u)
+    u1 = decoder._lr_bit_decision(l1)
+    print l1, '-->', u1
+
+    u = np.append(u, u1)
+    l2 = decoder._lr_decision_element(y, u)
+    u2 = decoder._lr_bit_decision(l2)
+    print l2, '-->', u2
+
+    u = np.append(u, u2)
+    l3 = decoder._lr_decision_element(y, u)
+    u3 = decoder._lr_bit_decision(l3)
+    print l3, '-->', u3
+
+    u = np.append(u, u3)
+    l4 = decoder._lr_decision_element(y, u)
+    u4 = decoder._lr_bit_decision(l4)
+    print l4, '-->', u4
+
+
+
+def decode2(decoder):
+    bits = np.ones(2)
+    enc = np.array([1, 1])
+
+    print enc
+    print decoder._lr_bit(enc[0]), decoder._lr_bit(enc[0])
+    l1 = decoder._lr_odd(decoder._lr_bit(enc[0]), decoder._lr_bit(enc[1]))
+    u1 = decoder._lr_bit_decision(l1)
+    print l1, '-->', u1
+
+    l2 = decoder._lr_even(decoder._lr_bit(enc[0]), decoder._lr_bit(enc[1]), u1)
+    u2 = decoder._lr_bit_decision(l2)
+    print l2, '-->', u2
+
+
+
 def main():
     power = 3
     n = 2 ** power
     k = 4
     frozenbits = np.zeros(n - k, dtype=int)
     frozenbitposition = np.array((0, 1, 2, 4), dtype=int)
+    frozenbitposition4 = np.array((0, 1), dtype=int)
 
 
     encoder = PolarEncoder(n, k, frozenbitposition, frozenbits, reverse=False)
@@ -145,7 +246,7 @@ def main():
     # print "llr=", llr
 
 
-    power = 2
+    power = 1
     n = 2 ** power
     nodes = np.arange(n, dtype=int)
     bit_reversed = decoder._vector_bit_reversed(nodes, power)
@@ -157,6 +258,9 @@ def main():
 
     print "This was our graph. Now see result"
     print graph
+
+    decode2(decoder)
+    decode4()
 
 
 

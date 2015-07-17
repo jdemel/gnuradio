6624c1b21695991a8092cdc1414f1e569f462d57
Johannes Demel <ufcsy@student.kit.edu>
2015-07-17 18:04:11 +0200

    polar: added commandline tool for channel construction

--- gr-fec/python/fec/polar/__init__.py
+++ gr-fec/python/fec/polar/__init__.py
@@ -23,11 +23,31 @@
 
 import channel_construction as cc
 from channel_construction_bec import bhattacharyya_bounds
+from helper_functions import is_power_of_two
+
+
+def get_z_params(is_prototype, channel, block_size, design_snr, mu):
+    print('POLAR code channel construction called with parameters channel={0}, blocksize={1}, design SNR={2}, mu={3}'.format(channel, block_size, design_snr, mu))
+    if not (channel == 'BSC' or channel == 'BEC'):
+        raise ValueError("channel is {0}, but only BEC and BSC are supported!".format(channel))
+    if not is_power_of_two(block_size):
+        raise ValueError("block size={0} is not a power of 2!".format(block_size))
+    if design_snr < -1.5917:
+        raise ValueError("design SNR={0} < -1.5917. MUST be greater!".format(design_snr))
+    if not mu > 0:
+        raise ValueError("mu={0} < 1. MUST be > 1!".format(mu))
+    if not is_prototype and channel == 'BSC':
+        z_params = cc.load_z_parameters(block_size, design_snr, mu)
+        print('Read Z-parameter file: {0}'.format(cc.default_dir() + cc.generate_filename(block_size, design_snr, mu)))
+        return z_params
+    return bhattacharyya_bounds(design_snr, block_size)
 
 
 def load_frozen_bits_info(is_prototype, channel, block_size, num_info_bits, design_snr, mu):
     num_frozen_bits = block_size - num_info_bits
-    z_params = bhattacharyya_bounds(design_snr, block_size)
+    if not mu > 0:
+        mu = 2
+    z_params = get_z_params(is_prototype, channel, block_size, design_snr, mu)
     data_set = {
         'positions': cc.get_frozen_bit_indices_from_z_parameters(z_params, num_frozen_bits),
         'values': [0, ] * num_frozen_bits,
@@ -38,12 +58,4 @@ def load_frozen_bits_info(is_prototype, channel, block_size, num_info_bits, desi
         'channel': channel,
         'mu': mu,
         }
-
-    if is_prototype:
-        return data_set
-
-    if channel == 'BSC':
-        z_params = cc.load_z_parameters(block_size, design_snr, mu)
-        data_set['positions'] = cc.get_frozen_bit_indices_from_z_parameters(z_params, num_frozen_bits)
-
     return data_set
